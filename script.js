7520490c80d9b1665dfeca02427fd57fcaba90be
Arthur <peneir20@gmail.com>
2026-05-27 20:35:08 -0300

    Avanços no a pousada

---
+++ script.js
@@ -0,0 +1,4 @@
+whatsb = document.querySelector("#whats")
+whatsb.addEventListener('click', function(event) {
+    window.location.href = "https://wa.me/5547920008202?text=Mensagem%20enviada%20pelo%20site"
+})
