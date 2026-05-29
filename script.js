709e50849a1e7176a2ebdc53524052a6a02060b2
Arthur <peneir20@gmail.com>
2026-05-29 11:07:01 -0300

    Iniciado drag em ambientes e outras mudanças de transição

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
+body = document.querySelector("body")
+header = document.querySelector("#header")
+amb_slide = document.querySelector("#amb_slide")
+
 whatsb = document.querySelector("#whats")
 whatsb.addEventListener('click', function(event) {
     window.location.href = "https://wa.me/5547920008202?text=Mensagem%20enviada%20pelo%20site"
 })
+
+window.addEventListener("scroll",function(event) {
+    if(window.scrollY > 100) {
+        header.dataset.transparent = "0"
+    } else {
+        header.dataset.transparent = "1"
+    }
+})
+
+let isDragging = false;
+let offsetX, offsetY;
+
+amb_slide.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    offsetX = e.clientX - amb_slide.offsetLeft;
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+    amb_slide.style.left = `${e.clientX - offsetX}px`;
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+    let l = amb_slide.style.left
+    l = l.slice(0,l.length-2)
+    l = pxToVh(l)
+    console.log(l)
+    if(amb_slide.style.left > 0) {
+        amb_slide.style.left = "0vw"
+    }else if (amb_slide.style.left < -38){
+        amb_slide.style.left = "38vw"
+    }else {
+        if(l%19 >= 9.5) {
+            amb_slide.style.left = `${Math.ceil(l/19)*19}vw`
+        } else {
+            amb_slide.style.left = `${Math.floor(l/19)*19}vw`
+        }
+    }
+    
+});
+
+function pxToVh(px) {
+    const viewportHeight = window.innerHeight;
+    return (px * 100) / viewportHeight;
+}
---
+++ todolist.md
@@ -0,0 +1,18 @@
+# Cabeçalho
+- Transição de cor
+- Espaçamento
+- Espaçamento da logo
+- Transição do underline
+
+# Geral
+- Tamanho do Whatsapp
+- View completa da imagem
+
+# Home:
+- Deslize das fotos
+- Botões das fotos
+
+# A pousada
+- Zoom nas imagens
+- Responsividade afinando as imagens mantendo a altura
+
