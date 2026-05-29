4e1fed05fab134aaca0a880c376bcd181c7e1228
Arthur <peneir20@gmail.com>
2026-05-29 13:10:05 -0300

    Melhorias no drag, Iframe do maps, outras mudanças pequenas

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 body = document.querySelector("body")
 header = document.querySelector("#header")
 amb_slide = document.querySelector("#amb_slide")
+ambient_rb = document.querySelector("#ambient_rb")
+ambient_lb = document.querySelector("#ambient_lb")
 
 whatsb = document.querySelector("#whats")
 whatsb.addEventListener('click', function(event) {
@@ -20,7 +22,7 @@ let offsetX, offsetY;
 
 amb_slide.addEventListener('mousedown', (e) => {
     isDragging = true;
-    offsetX = e.clientX - amb_slide.offsetLeft;
+    offsetX = e.clientX  - amb_slide.offsetLeft;
 });
 
 document.addEventListener('mousemove', (e) => {
@@ -29,24 +31,53 @@ document.addEventListener('mousemove', (e) => {
 });
 
 document.addEventListener('mouseup', () => {
-    isDragging = false;
-    let l = amb_slide.style.left
-    l = l.slice(0,l.length-2)
-    l = pxToVh(l)
-    console.log(l)
-    if(amb_slide.style.left > 0) {
-        amb_slide.style.left = "0vw"
-    }else if (amb_slide.style.left < -38){
-        amb_slide.style.left = "38vw"
-    }else {
-        if(l%19 >= 9.5) {
-            amb_slide.style.left = `${Math.ceil(l/19)*19}vw`
-        } else {
-            amb_slide.style.left = `${Math.floor(l/19)*19}vw`
+    if(isDragging) {
+        isDragging = false;
+        let l = get_amb_slide()
+        if(l > 0) {
+            amb_slide.style.left = "0vw"
+        }else if (l < -38){
+            amb_slide.style.left = "-38vw"
+        }else {
+            if(l%19 <= -9.5) {
+                amb_slide.style.left = `${Math.floor(l/19)*19}vw`
+            } else {
+                amb_slide.style.left = `${Math.ceil(l/19)*19}vw`
+            }
         }
     }
-    
 });
+
+ambient_rb.addEventListener("click", () => {
+    if(!isDragging) {
+        l = get_amb_slide_vw()
+        if(l >= -19) {
+            amb_slide.style.left = `${l-19}vw`
+        }
+    }
+})
+
+ambient_lb.addEventListener("click", () => {
+    if(!isDragging) {
+        l = get_amb_slide_vw()
+        if(l <= -19) {
+            amb_slide.style.left = `${(Number(l)+19)}vw`
+        }
+    }
+})
+
+function get_amb_slide() {
+    let l = amb_slide.style.left
+        l = l.slice(0,l.length-2)
+        l = pxToVh(l)
+    return l
+}
+
+function get_amb_slide_vw() {
+    let l = amb_slide.style.left
+        l = l.slice(0,l.length-2)
+    return l
+}
 
 function pxToVh(px) {
     const viewportHeight = window.innerHeight;
