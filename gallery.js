d5af84d7386bdbf5bc34ba1c8035e61b1b61f847
Arthur <peneir20@gmail.com>
2026-06-10 09:49:10 -0300

    Correções, iniciado melhor underline do cabeçalho

--- gallery.js
+++ gallery.js
@@ -10,6 +10,9 @@ for(let i = 1; i < 32; i++) {
     n.id = "gallery_"+i
     n.querySelector("img").src="assets/gallery/"+i+".webp"
     n.querySelector("img").dataset.img_group_order= String(i-1)
+    if(i >= 7) {
+        n.style.display = "none"
+    }
     n.addEventListener("click", function () {
         expandImage(3, i-1)
     })
@@ -17,9 +20,14 @@ for(let i = 1; i < 32; i++) {
 }
 
 gallery_see.addEventListener("click",function() {
+    gallery_see.textContent = "Ver mais"
+    p = "none"
     show = !show
-    p = show ? "block" : "none"
-    for(i = 6; i < 32; i++) {
+    if(show) {
+        gallery_see.textContent = "Ver menos"
+        p = "block"
+    }
+    for(i = 7; i < 32; i++) {
         gallery.querySelector("#gallery_"+i).style.display = p
     }
 })
