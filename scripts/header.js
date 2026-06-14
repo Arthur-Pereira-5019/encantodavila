ecf91f643de12343021bfbf8cbe2afabc81c25b4
Arthur <peneir20@gmail.com>
2026-06-14 19:42:55 -0300

    Negrito no cabeçalho

--- scripts/header.js
+++ scripts/header.js
@@ -8,16 +8,18 @@ eve_underline = document.querySelector("#eve_underline")
 gal_underline = document.querySelector("#gal_underline")
 con_underline = document.querySelector("#con_underline")
 
+button_home = document.querySelector("#button_home")
+button_pousada = document.querySelector("#button_pousada")
+button_ambients = document.querySelector("#button_ambients")
+button_acomodacoes = document.querySelector("#button_acomodacoes")
+button_gastronomia = document.querySelector("#button_gastronomia")
+button_eventos = document.querySelector("#button_eventos")
+button_galeria = document.querySelector("#button_galeria")
+button_contatos = document.querySelector("#button_contatos")
+current_level = -1
+
 window.addEventListener("scroll", function (event) {
     y = window.scrollY
-    hom_underline.dataset.active_underline = "0"
-    pou_underline.dataset.active_underline = "0"
-    amb_underline.dataset.active_underline = "0"
-    aco_underline.dataset.active_underline = "0"
-    gas_underline.dataset.active_underline = "0"
-    eve_underline.dataset.active_underline = "0"
-    gal_underline.dataset.active_underline = "0"
-    con_underline.dataset.active_underline = "0"
     if (isMobile) {
         home_bg_lb.style.opacity = 0
         home_bg_rb.style.opacity = 0
@@ -36,10 +38,43 @@ window.addEventListener("scroll", function (event) {
                 home_bg_rb.style.opacity = 1
             }
         }
-        hom_underline.dataset.active_underline = "1"
-    } else if (y < 1680) {
-        pou_underline.dataset.active_underline = "1"
+        if(current_level != 0) {
+            switchLevel(0)
+            hom_underline.dataset.active_underline = "1"
+            button_home.style.fontWeight = "600"
+        }
+    } else if (y < 1480) {
+        if(current_level != 1) {
+            switchLevel(1)
+            pou_underline.dataset.active_underline = "1"
+            button_pousada.style.fontWeight = "600"
+        }
     } else if (y < 2520) {
-        amb_underline.dataset.active_underline = "1"
+        if(current_level != 2) {
+            switchLevel(2)
+            amb_underline.dataset.active_underline = "1"
+            button_ambients.style.fontWeight = "600"
+        }
     }
 })
+
+function switchLevel(nl) {
+    hom_underline.dataset.active_underline = "0"
+    pou_underline.dataset.active_underline = "0"
+    amb_underline.dataset.active_underline = "0"
+    aco_underline.dataset.active_underline = "0"
+    gas_underline.dataset.active_underline = "0"
+    eve_underline.dataset.active_underline = "0"
+    gal_underline.dataset.active_underline = "0"
+    con_underline.dataset.active_underline = "0"
+    
+    button_home.style.fontWeight = "400"
+    button_pousada.style.fontWeight = "400"
+    button_ambients.style.fontWeight = "400"
+    button_acomodacoes.style.fontWeight = "400"
+    button_gastronomia.style.fontWeight = "400"
+    button_eventos.style.fontWeight = "400"
+    button_galeria.style.fontWeight = "400"
+    button_contatos.style.fontWeight = "400"
+    current_level = nl
+}
